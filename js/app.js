80cef33f9acb58e8ab791b4581434f5380277c5b
sereendaqamsih <sereen.aldaqamsih@gmail.com>
2021-04-26 17:03:09 +0300

    save question 5

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
-let userName = prompt('Whats your name please?');
+let userName = prompt('Whats your name please?').toUpperCase();
 
-alert ('welcome '+ userName +' we wil ask you a few questions about Sereen Aldaqamsih.');
+alert ('Welcome '+ userName +' we wil ask you a few questions about Sereen Aldaqamsih.');
 
 let userApprove = prompt('Are you ready?').toLowerCase();
 
@@ -21,47 +21,49 @@ switch (userFrom)
 
 case 'yes':
              console.log('The user in Jordan');
-             alert ('same as me');
+             alert ('Sereen lives in Jordan too.');
          break;
 case 'y':
-              console.log('the user in jordan');
-              alert ('same as me');
+              console.log('The user in jordan');
+              alert ('Sereen lives in Jordan too.');
 
           break;
 case 'no':
-             console.log('the user outside Jordan ');
+             console.log('the user outside Jordan, Sereen lives in Jordan.');
          break;
 case 'n':
-             console.log('the user outside Jordan ');
+             console.log('the user outside Jordan,Sereen lives in Jordan.');
          break;
 
     default:
          console.log (userFrom);
+         console.log('the user outside Jordan,Sereen lives in Jordan.');
          break;
 }
 
 
-let userWork = prompt('am i engineer?').toLowerCase();
+let userWork = prompt('Is Sereen engineer?').toLowerCase();
 
 switch (userWork)
 {
     case 'yes':
-        console.log ('welcome engineer');
-        alert ("That's true, im electrical engineer");
+        console.log ("That's true, Im electrical  power engineer");
+        alert ("That's true, Im electrical  power engineer");
         break;
     case 'y':
-        console.log ('welcome engineer');
-        alert ("That's true, im electrical engineer");
+        console.log ("That's true, Im electrical  power engineer");
+        alert ("That's true, Im electrical  power engineer");
 
         break;
     case 'no':
-        console.log ('welcome');
+        console.log ('Sereen studied Electrical power engineering.');
         break;
     case 'n':
-        console.log ('welcome');
+        console.log ('Sereen studied Electrical power engineering.');
         break;
  default:
      console.log(userWork);
+     console.log ('Sereen studied Electrical power engineering.');
      break;
 }
 
@@ -78,12 +80,16 @@ switch (userExp)
         
 case 'no':
     alert ('I have 4 years experience as electrical engineer');
+    console.log("I have 4 years experience as electrical engineer");
+
      break;
     case 'n':
         alert ('I have 4 years experience as electrical engineer');
+        console.log("I have 4 years experience as electrical engineer");
     break;
     default:
         console.log(userExp);
+        console.log("I have 4 years experience as electrical engineer");
         break;}
 
  let userUni= prompt("Sereen graduated from Just university?").toLowerCase();
@@ -97,14 +103,41 @@ switch (userUni){
         break;
         case 'no':
             alert('Sereen graduted from Yarmouk University 2014');
+            console.log('sereen graduted from yarmouk university');
             break;
             case 'n':
                 alert('Sereen graduted from Yarmouk University 2014');
+                console.log('sereen graduted from yarmouk university');
                 break;
                 default:
                     console.log(userUni);
+                    console.log('sereen graduted from yarmouk university');
                     break;
 }
 
-
-}
+let userChild= prompt("Is Sereen a mother?").toLowerCase();
+switch (userChild){
+    case 'yes':
+        alert('Sereen mother for one child,her name Yara');
+        console.log('Sereen mother for one child,her name Yara');
+        break;
+        case 'y':
+            alert('Sereen mother for one child,her name Yara');
+            console.log('Sereen mother for one child,her name Yara');
+            break;
+            case 'no':
+         
+           console.log('Sereen mother for one child,her name Yara');
+           break;
+           case 'n':
+               console.log('Sereen mother for one child,her name Yara');
+               break;
+               default:
+                   console.log(userChild);
+                   console.log('Sereen mother for one child,her name Yara');
+                   break;
+        }
+    
+    alert( 'welcome '+userName+', Thank you for your time.');
+    break;
+    }
