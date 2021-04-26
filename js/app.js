b7879a8e4ef40f769327ed386b5cbcb851dfba3f
sereendaqamsih <sereen.aldaqamsih@gmail.com>
2021-04-26 15:55:23 +0300

    save question 2

--- js/app.js
+++ js/app.js
@@ -7,9 +7,12 @@ switch (userFrom)
 
 case 'yes':
              console.log('The user in Jordan');
+             alert ('same as me');
          break;
 case 'y':
               console.log('the user in jordan');
+              alert ('same as me');
+
           break;
 case 'no':
              console.log('the user outside Jordan ');
@@ -21,4 +24,29 @@ case 'n':
     default:
          console.log (userFrom);
          break;
+}
+
+
+let userWork = prompt('am i engineer?').toLowerCase();
+
+switch (userWork)
+{
+    case 'yes':
+        console.log ('welcome engineer');
+        alert ("That's true, im electrical engineer");
+        break;
+    case 'y':
+        console.log ('welcome engineer');
+        alert ("That's true, im electrical engineer");
+
+        break;
+    case 'no':
+        console.log ('welcome');
+        break;
+    case 'n':
+        console.log ('welcome');
+        break;
+ default:
+     console.log(userWork);
+     break;
 }
